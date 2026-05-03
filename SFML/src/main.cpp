a670353bd9c1171023f49c7e43ce41c336960651
GizzlyFacu <pamdulxe@gmail.com>
2026-05-03 00:27:48 -0300

    SFML changes

--- SFML/src/main.cpp
+++ SFML/src/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(){
-	std::cout<<"brr brr";
-	return 0;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Square");
+
+    sf::RectangleShape square({100.f, 100.f}); // tamaño (ancho, alto)
+    square.setFillColor(sf::Color::Green);
+    square.setPosition({350.f, 250.f});
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(square);
+        window.display();
+    }
 }
